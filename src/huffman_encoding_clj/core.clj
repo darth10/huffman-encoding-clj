c9cb0ed2ecf7ab27943d2cd246ac1286ab53c695
Akhil Wali <akhil.wali.10@gmail.com>
2013-01-12 19:43:11 +0530

    Implemented make-code-tree. Still needs tests.

--- src/huffman_encoding_clj/core.clj
+++ src/huffman_encoding_clj/core.clj
@@ -11,7 +11,6 @@
 (defmethod print-method Fork
   [fork w]
   (let [this (str " "
-                  (apply str (seq (:chars fork))) "|"
                   (:weight fork)
                   " ")]
     (print-simple "<" w)
@@ -30,15 +29,6 @@
   [node c] (or (has-char? (:left node) c)
                (has-char? (:right node) c)))
 
-(defmulti weight-sum class)
-
-(defmethod weight-sum Leaf
-  [node] (:weight node))
-
-(defmethod weight-sum Fork
-  [node] (+ (weight-sum (:left node))
-            (weight-sum (:right node))))
-
 (defmulti chars-sum class)
 
 (defmethod chars-sum Leaf
@@ -53,13 +43,11 @@
   [[c w]] (->Leaf c w))
 
 (defn make-fork [t1 t2]
-  (let [weight1 (weight-sum t1)
-        weight2 (weight-sum t2)
+  (let [weight1 (:weight t1)
+        weight2 (:weight t2)
         w-sum (+ weight1 weight2)
-        c-sum (concat
-               (chars-sum t1)
-               (chars-sum t2))
-        [left right] (if (< weight1 weight2) 
+        c-sum (concat (chars-sum t1) (chars-sum t2))
+        [left right] (if (< weight1 weight2)
                        [t1 t2]
                        [t2 t1])]
     (->Fork left right c-sum w-sum)))
--- src/huffman_encoding_clj/trees.clj
+++ src/huffman_encoding_clj/trees.clj
@@ -14,9 +14,19 @@
 (defn make-leaf-list [char-map]
   (map make-leaf char-map))
 
-;; TODO not sure this really works. read theory and use tests from patmat
+(defn has-lesser-weight? [weight]
+  #(< (:weight %) weight))
+
+(defn reduce-tree-list [trees]
+  (if (= 1 (count trees))
+    (first trees)
+    (let [[t1 t2] (take 2 trees)
+          other-trees (drop 2 trees)
+          new-tree (make-fork t1 t2)
+          new-weight (:weight new-tree)
+          [ts1 ts2] (partition-by (has-lesser-weight? new-weight) other-trees)]
+      (recur (concat ts1 [new-tree] ts2)))))
+
 (defn make-code-tree [str]
-  (let [leaf-list (make-leaf-list
-                   (get-char-map str))]
-    (reduce #(make-fork %1 %2)
-            leaf-list)))
+  (let [leaf-list (make-leaf-list (get-char-map str))]
+    (reduce-tree-list leaf-list)))
